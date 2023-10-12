35e794ccb6f1bdf0a92cdb252bf8516ceccfc55e
Furkan Doğan <furkanaxx34@gmail.com>
2023-10-13 01:04:12 +0300

    polish.

--- handler/upload_handler.go
+++ handler/upload_handler.go
@@ -1,78 +1,78 @@
 package handler
 
 import (
-	"videohub/util"
-	"videohub/model"
-	"videohub/config"
-	"path/filepath"
-	"io"
-	"fmt"
-	"net/http"
-	"os"
-	"go.mongodb.org/mongo-driver/mongo"
-	"log"
-	"strings"
+   "videohub/util"
+   "videohub/model"
+   "videohub/config"
+   "path/filepath"
+   "io"
+   "fmt"
+   "net/http"
+   "os"
+   "go.mongodb.org/mongo-driver/mongo"
+   "log"
+   "strings"
 )
 
 type UploadHandler struct {
-	MongoCollection *mongo.Collection
+   MongoCollection *mongo.Collection
 }
 
 func (u *UploadHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	file, header, err := r.FormFile("file")
-	if err != nil {
-		http.Error(w, "Unable to read uploaded file", http.StatusBadRequest)
-		return
-	}
-	defer file.Close()
+   file, header, err := r.FormFile("file")
+   if err != nil {
+      http.Error(w, "Unable to read uploaded file", http.StatusBadRequest)
+      return
+   }
+   defer file.Close()
 
-	id := r.FormValue("id")
-	title := r.FormValue("title")
+   id := r.FormValue("id")
+   title := r.FormValue("title")
 
-	if title == "" {
-		http.Error(w, "Title cannot be null", http.StatusBadRequest)
-		return
-	}
+   if title == "" {
+      http.Error(w, "Title cannot be null", http.StatusBadRequest)
+      return
+   }
 
-	fileExtension := strings.TrimPrefix(filepath.Ext(header.Filename), ".")
-	targetPath := fmt.Sprintf("%s/%s/%s%s", config.Config.VideosDir, fileExtension, id, filepath.Ext(header.Filename))
-	err = os.MkdirAll(filepath.Dir(targetPath), 0755)
-	if err != nil {
-		http.Error(w, "Failed to create directory", http.StatusInternalServerError)
-		return
-	}
+   fileExtension := strings.TrimPrefix(filepath.Ext(header.Filename), ".")
+   targetPath := fmt.Sprintf("%s/%s/%s%s", config.Config.VideosDir, fileExtension, id, filepath.Ext(header.Filename))
+   err = os.MkdirAll(filepath.Dir(targetPath), 0755)
+   if err != nil {
+      http.Error(w, "Failed to create directory", http.StatusInternalServerError)
+      return
+   }
 
-	targetFile, err := os.Create(targetPath)
-	if err != nil {
-		http.Error(w, "Failed to create target file", http.StatusInternalServerError)
-		return
-	}
-	defer targetFile.Close()
+   targetFile, err := os.Create(targetPath)
+   if err != nil {
+      http.Error(w, "Failed to create target file", http.StatusInternalServerError)
+      return
+   }
+   defer targetFile.Close()
 
-	_, err = io.Copy(targetFile, file)
-	if err != nil {
-		http.Error(w, "Failed to copy to target file", http.StatusInternalServerError)
-		return
-	}
+   _, err = io.Copy(targetFile, file)
+   if err != nil {
+      http.Error(w, "Failed to copy to target file", http.StatusInternalServerError)
+      return
+   }
 
-	video := model.Video{
-		ID:    id,
-		Title: title,
-		VideoUrl: fmt.Sprintf("%s/%s.%s", fileExtension, id, fileExtension),
-	}
+   video := model.Video{
+      ID:    id,
+      Title: title,
+      VideoUrl: fmt.Sprintf("%s/%s.%s", fileExtension, id, fileExtension),
+   }
 
-	err = util.InsertVideoToDB(u.MongoCollection, video)
-	if err != nil {
-		log.Printf("Failed to insert video to MongoDB: %v", err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
+   err = util.InsertVideoToDB(u.MongoCollection, video)
+   if err != nil {
+      log.Printf("Failed to insert video to MongoDB: %v", err)
+      http.Error(w, "Internal server error", http.StatusInternalServerError)
+      return
+   }
 
-	scheme := "http"
-	if r.TLS != nil || r.Header.Get("X-Forwarded-Proto") == "https" {
-	    scheme = "https"
-	}
+   scheme := "http"
+   if r.TLS != nil || r.Header.Get("X-Forwarded-Proto") == "https" {
+       scheme = "https"
+   }
 
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Upload successful. Video path: %s", scheme + "://" + r.Host + "/" + targetPath)))
+   w.WriteHeader(http.StatusOK)
+   w.Write([]byte(fmt.Sprintf("Upload successful. Video path: %s", scheme + "://" + r.Host + "/" + targetPath)))
 }
--- handler/videos_handler.go
+++ handler/videos_handler.go
@@ -1,43 +1,43 @@
 package handler
 
 import (
-	"videohub/util"
-	"encoding/json"
-	"net/http"
-	"go.mongodb.org/mongo-driver/mongo"
-	"strconv"
+   "videohub/util"
+   "encoding/json"
+   "net/http"
+   "go.mongodb.org/mongo-driver/mongo"
+   "strconv"
 )
 
 type VideoHandler struct {
-	MongoCollection *mongo.Collection
+   MongoCollection *mongo.Collection
 }
 
 func (vh *VideoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	page, _ := strconv.Atoi(r.URL.Query().Get("page"))
-	pageSize, _ := strconv.Atoi(r.URL.Query().Get("pageSize"))
+   page, _ := strconv.Atoi(r.URL.Query().Get("page"))
+   pageSize, _ := strconv.Atoi(r.URL.Query().Get("pageSize"))
 
-	if page <= 0 || pageSize <= 0 {
-		http.Error(w, "Invalid page or pageSize parameter", http.StatusBadRequest)
-		return
-	}
+   if page <= 0 || pageSize <= 0 {
+      http.Error(w, "Invalid page or pageSize parameter", http.StatusBadRequest)
+      return
+   }
 
-	scheme := "http"
-	if r.TLS != nil || r.Header.Get("X-Forwarded-Proto") == "https" {
-	    scheme = "https"
-	}
+   scheme := "http"
+   if r.TLS != nil || r.Header.Get("X-Forwarded-Proto") == "https" {
+       scheme = "https"
+   }
 
-	videos, err := util.GetAllVideosFromDB(vh.MongoCollection, page, pageSize, scheme + "://" + r.Host)
-	if err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
+   videos, err := util.GetAllVideosFromDB(vh.MongoCollection, page, pageSize, scheme + "://" + r.Host)
+   if err != nil {
+      http.Error(w, "Internal server error", http.StatusInternalServerError)
+      return
+   }
 
-	jsonResponse, err := json.Marshal(videos)
-	if err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
+   jsonResponse, err := json.Marshal(videos)
+   if err != nil {
+      http.Error(w, "Internal server error", http.StatusInternalServerError)
+      return
+   }
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsonResponse)
+   w.Header().Set("Content-Type", "application/json")
+   w.Write(jsonResponse)
 }
